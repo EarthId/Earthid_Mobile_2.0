57f4af30b97883eb83994737908f049b6fe9c68f
Vaibhav <vaibhavahire123@gmail.com>
2024-09-30 21:04:13 +0530

    Updated Earthid build

--- android/app/src/main/java/com/earthid/MainActivity.kt
+++ android/app/src/main/java/com/earthid/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.globalidiqtechandroid
+package com.earthid
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/earthid/MainApplication.kt
+++ android/app/src/main/java/com/earthid/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.globalidiqtechandroid
+package com.earthid
 
 import android.app.Application
 import com.facebook.react.PackageList
