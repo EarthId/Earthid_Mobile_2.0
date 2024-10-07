7eb07cebb913ecfa13d2590d771c2ec3c7901b48
Vaibhav <vaibhavahire123@gmail.com>
2024-10-07 16:22:53 +0530

    Added globalID config

--- android/app/src/main/java/com/earthid/MainActivity.kt
+++ android/app/src/main/java/com/earthid/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.globalidiqtechandroid
+package com.globalidiqtechandroid1
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/earthid/MainApplication.kt
+++ android/app/src/main/java/com/earthid/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.globalidiqtechandroid
+package com.globalidiqtechandroid1
 
 import android.app.Application
 import com.facebook.react.PackageList
